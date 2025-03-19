f8e8c263e1c5b254305c11595e60538a0cb71e37
Jongmin Choi <138505370+JongminChoi98@users.noreply.github.com>
2025-03-18 21:33:16 -0700

    [ADD] products page

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <>
+      <h1>Shopping Cart Management</h1>
+      <ProductList />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import "./ProductList.css";
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+function ProductList() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const addToCart = (product: Product) => {
+    console.log(`${product.name} has been added to the cart.`);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
+  return (
+    <div className="product-list">
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id} className="product-item">
+            <img
+              src="/golf-1208900_1280.jpg"
+              alt={product.name}
+              className="product-image"
+            />
+            <div className="product-details">
+              <span className="product-name">{product.name}</span>
+              <span className="product-price">{product.price}$</span>
+            </div>
+            <button
+              onClick={() => addToCart(product)}
+              className="add-to-cart-button"
+            >
+              Add to Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductList;
